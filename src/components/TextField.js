9ede23dcaf6a8e5aa32d5bcc6324fcbad64a195e
MDmubarak786 <mohammedmubarakmk@gmail.com>
2022-01-16 19:49:12 +0530

    feat(password):ui changed as dot

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 
 const TextFieldBox = ({ label, onChange, value, styles }) => {
+  const newType = label === 'Password' ? 'password' : 'text'
   return (
     <div className={styles}>
       <TextField
@@ -11,7 +12,7 @@ const TextFieldBox = ({ label, onChange, value, styles }) => {
         sx={{ width: '25%' }}
         onChange={onChange}
         value={value}
-        type="text"
+        type={newType}
       />
     </div>
   )
