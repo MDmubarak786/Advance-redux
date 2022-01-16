50d0a46843eeb3ac7b40a30a3153d41e1a265614
Mohammed Mubarak <44917891+MDmubarak786@users.noreply.github.com>
2022-01-16 19:51:11 +0530

    Revert "feat(password): ui changed as dot"

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,7 +2,6 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 
 const TextFieldBox = ({ label, onChange, value, styles }) => {
-  const newType = label === 'Password' ? 'password' : 'text'
   return (
     <div className={styles}>
       <TextField
@@ -12,7 +11,7 @@ const TextFieldBox = ({ label, onChange, value, styles }) => {
         sx={{ width: '25%' }}
         onChange={onChange}
         value={value}
-        type={newType}
+        type="text"
       />
     </div>
   )
